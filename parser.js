2972175218a4e9216d1cc3e9a9566aaffa80d12f
Kevin <korlando@redventures.com>
2017-11-11 00:09:24 -0500

    gather more geocode data

--- parser.js
+++ parser.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const inputjson = require('./data/cleanData.json');
 const fetch = require('node-fetch');
 const returnArray = inputjson.map(async (value) => {
-  if (value.id < 5000) {
-    const result = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${value.where}&key=AIzaSyBXIjg6-Mzg5XY1_nUNyyjjtA6zKGyu6H4`);
+  if (value.id > 15000 && value.id < 17000) {
+    const result = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${value.where}&key=AIzaSyC5_0aZRIIEf-B7_4fgtJqIb2CBpBRnp6A`);
     const resultJson = await result.json();
     if (resultJson.status === 'OK') {
       const results = resultJson.results;
@@ -18,7 +18,7 @@ const returnArray = inputjson.map(async (value) => {
 });
 
 Promise.all(returnArray).then((res) => {
-  fs.writeFileSync('outputData.json', JSON.stringify(res));
+  fs.writeFileSync('outputData10.json', JSON.stringify(res));
 });
 
 
