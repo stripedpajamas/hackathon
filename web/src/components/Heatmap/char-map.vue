a1ebfadb66451a6b4bdf0c20ce1f4f8bcebf1373
Kevin <korlando@redventures.com>
2017-11-11 10:48:05 -0500

    adding location data json and adding weights to heat map

--- web/src/components/Heatmap/char-map.vue
+++ web/src/components/Heatmap/char-map.vue
@@ -11,26 +11,36 @@ export default {
     return {};
   },
   mounted() {
-    // get json data in here
-    const locations = locationData.coordinates;
+    fetch('https://hackathon-ysfkztzsac.now.sh/machine', {
+      method: 'post',
+      body: JSON.stringify({
+        input: locationData,
+      }),
+    }).then(response => response.json())
+    .then((data) => {
+      // get json data in here
+      const locations = locationData.coordinates;
 
-    const heatmapData = [];
-    locations.forEach(location =>
-      heatmapData.push(new window.google.maps.LatLng(location[0], location[1])));
+      const heatmapData = [];
+      locations.forEach((location, index) =>
+        heatmapData.push({
+          location: new window.google.maps.LatLng(location[0], location[1]), weight: data[index][0],
+        }));
 
-    const charlotte = new window.google.maps.LatLng(35.2271, -80.8431);
+      const charlotte = new window.google.maps.LatLng(35.2271, -80.8431);
 
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: charlotte,
-      zoom: 13,
-      mapTypeId: 'terrain',
+      const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: charlotte,
+        zoom: 13,
+        mapTypeId: 'terrain',
+      });
+
+      const heatmap = new window.google.maps.visualization.HeatmapLayer({
+        data: heatmapData,
+      });
+
+      heatmap.setMap(map);
     });
-
-    const heatmap = new window.google.maps.visualization.HeatmapLayer({
-      data: heatmapData,
-    });
-
-    heatmap.setMap(map);
   },
 };
 </script>
