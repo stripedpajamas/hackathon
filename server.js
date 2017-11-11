e25ea4aa143443ad43834807a4c8ea9da7468672
peter squicciarini <peter.j.squicciarini@gmail.com>
2017-11-11 08:37:41 -0500

    Server stuff

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const body = require('body-parser');
 const machine = require('./machine');
 
 const app = express();
+app.use(body.json());
 
 app.get('/', express.static('./web/dist'));
-app.get('/machine', (req, res) => {
+app.post('/machine', (req, res) => {
   // make properly formatted input from req.body
-  let input;
-  res.end(machine(input));
+  const input = req.body.input;
+  let output = [];
+  if (Array.isArray(req.body.input)) {
+    output = input.map(x => machine(x));
+  }
+  res.end(output);
 });
+
+app.listen(process.env.PORT || 3000);
